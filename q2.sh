e8cd06f131fd4b568f44e90ad2da4ff6e32b1db5
dlbb-w <72599729+dlbb-w@users.noreply.github.com>
2020-10-19 16:00:26 +0800

    Update q2.sh

--- q2.sh
+++ q2.sh
@@ -2,10 +2,15 @@
 #!/bin/bash
 for i in {1..100}
 do
+        #make a directory and enter it
 	mkdir DDM$i;cd DDM$i
+	#create time_till_now.txt and edit it by EOF
 	cat>>./time_till_now.txt<<EOF
+#writing the content of this txt
 nanoseconds since 1970-01-01 00:00:00 UTC:
 <$(date +%s%N)>
+#finish the writing
 EOF
+#quit the directory
 cd
 done
